fcc81203400c04e19617f5869c00a2be9db80138
davis930 <116705627+davis930@users.noreply.github.com>
2023-05-24 00:04:41 -0500

    Update Mining Twitter Data Part 1 Collecting data.py

--- Mining Twitter Data Part 1 Collecting data.py
+++ Mining Twitter Data Part 1 Collecting data.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = "H4VJP8RGGXge44FhiA0IDbSFc"
-consumer_secret = "oasFMmJrmpDGhafqCwz87QA9Y0ZLiX1ZCsDFxEPU1fiupvV1CP"
-access_token = "764283896875843585-BCjE7PpBCXP79XErClu9ufuNJzdu1LP"
-access_secret = "L8Axk7IfqDt2HfH4E5BCH98S9d1P93z402CZF99fPIPCf"
+consumer_key = "..."
+consumer_secret = "..."
+access_token = "..."
+access_secret = "..."
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
